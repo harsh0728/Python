3431c0eb283583077106979bac1dcb9d14f35ea0
Harsh Kumar <hk.develop07@gmail.com>
2026-06-06 00:08:31 +0530

    operators

---
+++ arithmetic.py
@@ -0,0 +1,43 @@
+#arithmetic operators
+
+a=10
+b=5
+
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a/b)
+print("a//b=",a//b) #floor division
+print("a%b=",a%b) #modulo
+print("a**b=",a**b) #exponentiation a^b
+
+#relational operators
+a=50
+b=20
+
+print("a==b",a==b)
+print("a!=b",a!=b)
+print("a>b",a>b)
+print("a<b",a<b)
+print("a>=b",a>=b)
+print("a<=b",a<=b)
+
+#Assignment operators
+a=10
+a+=5 #a=a+5
+print("a=",a)
+a-=3 #a=a-3
+print("a=",a)
+a*=2 #a=a*2
+print("a=",a)
+a/=4 #a=a/4
+print("a=",a)
+a//=2 #a=a//2
+print("a=",a)
+
+#Logical operators
+x=True
+y=False
+print("x and y =",x and y)
+print("x or y =",x or y)
+print("not x =",not x)
