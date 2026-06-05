314201ee73b4a8ba0a3e0e04260c038f0e0bd03c
Harsh Kumar <hk.develop07@gmail.com>
2026-06-05 22:58:08 +0530

    variables

--- Variables and Data Types/main.py
+++ Variables and Data Types/main.py
@@ -1,3 +1,10 @@
 print("Hello, World!")
+
+name="Harsh"
+age=23
+price=28.44
+
+print("My name is ",name,"and I am ",age," years old. The price is ",price)
+
 text=input("Type something: ")
 print(f"You entered: {text}")
