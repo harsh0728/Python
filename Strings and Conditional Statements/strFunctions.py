4eeb70b498322ea0969edb16c50ca4be933cff4a
Harsh Kumar <hk.develop07@gmail.com>
2026-06-06 23:40:52 +0530

    string operations

---
+++ Strings and Conditional Statements/strFunctions.py
@@ -0,0 +1,16 @@
+str="i am studyoing Python by myself"
+
+# Ends With
+print ("endswith: ",str.endswith("elf"))
+
+# Capitalize
+print ("capitalize: ",str.capitalize())
+
+# Replace
+print ("replace: ",str.replace("y", "z"))
+
+# Find
+print ("find: ",str.find("Python"))
+
+# Count
+print ("count: ",str.count("y"))
