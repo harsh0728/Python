eaba15ee616acc5e8061f29d8d6308c2e487044d
Harsh Kumar <hk.develop07@gmail.com>
2026-06-06 22:43:33 +0530

    Average and Greater

---
+++ practice questions/3.py
@@ -0,0 +1,3 @@
+first=float(input("Enter the first number: "))
+second=float(input("Enter the second number: "))
+print("Average of the two numbers is: ", (first+second)/2)
---
+++ practice questions/4.py
@@ -0,0 +1,6 @@
+first=int(input("Enter the first number: "))
+second=int(input("Enter the second number: "))
+if (first>=second):
+    print("True")
+else: 
+    print("False")
