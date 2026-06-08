7a2735f62a20003a4f14e7a0d0f66765d3efd334
Harsh Kumar <hk.develop07@gmail.com>
2026-06-08 11:56:53 +0530

    for loop

--- Loops/loop.py
+++ Loops/loop.py
@@ -1,5 +1,22 @@
+# while loop
 count=1
 
 while count<=5:
     print(count)
     count+=1
+
+# for loop
+nums=[1,2,3,4,5]
+
+for num in nums:
+    print (num)
+
+str="Hello, World!"
+
+for char in str:
+    if (char=='o'):
+        print("Found 'o' at index: ",str.index(char))
+        break
+    print(char)
+else:
+    print("Character 'o' not found in the string.")
